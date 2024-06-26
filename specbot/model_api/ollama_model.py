411af0cee946df457a4e45b8b49dace2200f7d15
SemPhares <elsempharesyaovi@gmail.com>
2024-06-26 16:15:51 +0200

    Update ollama_model.py with prompt function for generating user message

--- specbot/model_api/ollama_model.py
+++ specbot/model_api/ollama_model.py
@@ -1,5 +1,22 @@
 from langchain_community.chat_models.ollama import ChatOllama
+from langchain_core.messages import HumanMessage
 from .llm_typing import llm_input, llm_output, llm_image_input
+
+
+def prompt_func(text:str, image_bs4 ):
+    image_part = {
+        "type": "image_url",
+        "image_url": f"data:image/jpeg;base64,{image_bs4}",
+    }
+
+    content_parts = []
+
+    text_part = {"type": "text", "text": text}
+
+    content_parts.append(image_part)
+    content_parts.append(text_part)
+
+    return [HumanMessage(content=content_parts)]
 
 
 def ollama_caption_image(query:llm_image_input) -> llm_output:
@@ -14,25 +31,17 @@ def ollama_caption_image(query:llm_image_input) -> llm_output:
                        repeat_penalty = 1.2,
                        top_p = 0.7)
     
-    # messages=[
-    #         {
-    #             'role': 'user',
-    #             'content': query.input,
-    #             'images': [query.image_bs4]
-    #         }
-    #     ]
-    
-    messages = [
+    message = [
             {
-                "role": "user",
-                "content": [
-                    {"type" : "text", "text": query.input},
-                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{query.image_bs4}" } }
-                ]
+                'role': 'user',
+                'content': query.input,
+                'images': [query.image_bs4]
             }
         ]
+    
+    # message = prompt_func(query.input, query.image_bs4)
 
-    output = model.invoke(messages)
+    output = model.invoke(message)
     output = llm_output(response = str(output.content), 
                         model_name = query.model_name)
     return output
